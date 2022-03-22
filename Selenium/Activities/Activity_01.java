366bafea1f70f9d4c5549da85b26f18c63515d50
MonicaKhushalani <101171793+MonicaKhushalani@users.noreply.github.com>
2022-03-22 17:18:49 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity_01.java
@@ -0,0 +1,14 @@
+package Demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity_01 {
+    public static void main(String[] args){
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.training-support.net");
+        driver.close();
+
+
+    }
+}
