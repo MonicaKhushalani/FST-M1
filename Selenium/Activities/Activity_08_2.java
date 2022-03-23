bb6ea657a1f3803a5f4fcc081e4a7994fec65d29
MonicaKhushalani <101171793+MonicaKhushalani@users.noreply.github.com>
2022-03-23 19:41:14 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity_08_2.java
@@ -0,0 +1,42 @@
+package Demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+
+public class Activity_08_2 {
+
+    public  static void main(String[] args){
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://training-support.net/selenium/tables");
+
+        //Get columns
+        List<WebElement> columns = driver.findElements(By.xpath("//table[@id='sortableTable']/thead/tr/th"));
+        //Get rows
+        List<WebElement> rows = driver.findElements(By.xpath("//table[@id='sortableTable']/tbody/tr"));
+
+        System.out.println("The size of the Columns : " + columns.size());
+        System.out.println("The size of the Rows : " + rows.size());
+
+        WebElement cellValueBefore = driver.findElement(By.xpath("//table[@id='sortableTable']/tbody/tr[2]/td[2]"));
+        System.out.println("Second row, second column value(Before sorting): " + cellValueBefore.getText());
+
+        //Sort the table
+        driver.findElement(By.xpath("//table[@id='sortableTable']/thead/tr/th[2]")).click();
+
+        //Print the value again
+        WebElement cellValueAfter = driver.findElement(By.xpath("//table[@id='sortableTable']/tbody/tr[2]/td[2]"));
+        System.out.println("Second row, second column value(After sorting): " + cellValueAfter.getText());
+
+        //Print footer cell value
+        WebElement footer = driver.findElement(By.xpath("//table[@id='sortableTable']/tfoot/tr"));
+        System.out.println("Table footer values: " + footer.getText());
+        
+        driver.quit();
+
+    }
+}
